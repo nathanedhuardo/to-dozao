e689e3c7b2ec75fb9dfa391adff5acd70b2b63bc
Joaovitorrr07 <vitorjoaovitor2004@gmail.com>
2025-12-13 02:20:59 -0300

    removendo implementações de teste

--- src/main/java/br/edu/ufape/todozao/repository/TaskHistoryRepository.java
+++ src/main/java/br/edu/ufape/todozao/repository/TaskHistoryRepository.java
@@ -5,7 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskHistoryRepository extends JpaRepository<TaskHistory, Long> {//solicita um repositório para TaskHistory e o tipo da chave primária é Long
 
-
-
-
 }
